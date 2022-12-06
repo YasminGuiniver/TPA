8399ff1c79c144ae724c694ac44b81d884effc8d
Yasmin Guiniver <94757345+YasminGuiniver@users.noreply.github.com>
2022-12-06 17:10:29 -0300

    Add files via upload

---
+++ verificaPositivoNegativo.java
@@ -0,0 +1,19 @@
+import java.ultil.Scanner; 
+public class SinaldosNumeros { 
+
+    public static void main (String [] args ) { 
+
+     Scanner in = ler Scanner (System.in); 
+
+	//VARIAVEIS
+     	double numero; 
+     		System.out.println ("Digite um número"); 
+       		numero= in.nextDouble ;
+
+	//TOMADA DE DECISÃO
+        if(n>=0) {
+          System.out.println ("Esse número é positivo") 
+        }else { 
+           System.out.println ("Esse número é negativo"); 
+  } 
+} 
